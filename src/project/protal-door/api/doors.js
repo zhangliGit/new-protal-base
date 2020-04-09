5f937287129201202c0f0359986897c3cfc37348
zhangliGit <674937076@qq.com>
2020-04-09 21:38:31 +0800

    代码优化

--- src/project/protal-door/api/doors.js
+++ src/project/protal-door/api/doors.js
@@ -6,16 +6,16 @@
 import hostEnv from '@config/host-env'
 
 const doorsApi = {
-    getappointList: '/invite/appoint/list#post', // 访客记录
-    getappointDetail: '/invite/appoint/detail#get', // 详细访问记录
-    addcause: '/cause/info/add#post', // 事由添加
-    delcause: '/cause/info/delete#get', // 事由删除
-    getcauseList: '/cause/info/list#post', // 事由列表
-    addcontrolgroup: '/visitor/controlgroup/add#post', // 添加访客控制组
-    delcontrolgroup: '/visitor/controlgroup/delete#get', // 删除访客控制组
-    getcontrolgroupList: '/visitor/controlgroup/list#post' // 访客控制组查询列表
+  getappointList: '/invite/appoint/list#post', // 访客记录
+  getappointDetail: '/invite/appoint/detail#get', // 详细访问记录
+  addcause: '/cause/info/add#post', // 事由添加
+  delcause: '/cause/info/delete#get', // 事由删除
+  getcauseList: '/cause/info/list#post', // 事由列表
+  addcontrolgroup: '/visitor/controlgroup/add#post', // 添加访客控制组
+  delcontrolgroup: '/visitor/controlgroup/delete#get', // 删除访客控制组
+  getcontrolgroupList: '/visitor/controlgroup/list#post' // 访客控制组查询列表
 }
 for (const val in doorsApi) {
   doorsApi[val] = `${hostEnv.zx}${doorsApi[val]}`
 }
-export default doorsApi 
+export default doorsApi
