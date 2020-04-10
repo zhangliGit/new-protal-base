fa37955a0173125b6060aefd986eee303fb83b6c
unknown <824876766@qq.com>
2020-04-10 17:15:22 +0800

    代码提交

--- src/project/protal-attendance/api/attendanceApi.js
+++ src/project/protal-attendance/api/attendanceApi.js
@@ -11,7 +11,9 @@ const homeApi = {
   updateAccess: '/attendance/group/update#post', // 修改考勤组
   delAccess: '/attendance/group/delete#delQuery', // 删除考勤组
   bindAccessUser: '/attendance/group/bind/user#post', // 考勤组添加人员
-  getAccessDetail: '/attendance/group/detail#get' // 考勤组详情
+  getAccessDetail: '/attendance/group/detail#get', // 考勤组详情
+  getTeaRecordStatic: '/teacher/static/index#get', // 教师考勤统计
+  getStuRecordStatic: '/student/static/index#get' // 学生考勤统计
 }
 for (const val in homeApi) {
   homeApi[val] = `${hostEnv.lz}${homeApi[val]}`
