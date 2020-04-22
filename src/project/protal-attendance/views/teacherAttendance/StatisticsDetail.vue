de3c228478e65b297d19c37ac7990d49e68d976b
tiancheng <tiancheng@qq.com>
2020-04-22 15:14:48 +0800

    优化

--- src/project/protal-attendance/views/teacherAttendance/StatisticsDetail.vue
+++ src/project/protal-attendance/views/teacherAttendance/StatisticsDetail.vue
@@ -32,8 +32,8 @@
           <div>{{ dealTime.record.offWorkTime | gmtToDate }}</div>
         </template>
         <template v-slot:other2="onSnacpUrl">
-          <img :src="onSnacpUrl.onSnacpUrl" alt="">
-          <img :src="onSnacpUrl.offSnacpUrl" alt="">
+          <img style="width: 60px; height: 60px; display: inline-block;" :src="onSnacpUrl.record.onSnacpUrl" alt="">
+          <img style="width: 60px; height: 60px; display: inline-block;margin-left:10px;" :src="onSnacpUrl.record.offSnacpUrl" alt="">
         </template>
       </table-list>
       <page-num v-model="pageList" :total="total" @change-page="showList"></page-num>
@@ -67,7 +67,7 @@ const columns = [
   {
     title: '打卡时间',
     dataIndex: 'dealTime',
-    width: '45%',
+    width: '35%',
     scopedSlots: {
       customRender: 'other1'
     }
@@ -83,7 +83,7 @@ const columns = [
   {
     title: '抓拍照',
     dataIndex: 'onSnacpUrl',
-    width: '15%',
+    width: '25%',
     scopedSlots: {
       customRender: 'other2'
     }
