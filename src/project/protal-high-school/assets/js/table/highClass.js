42322f02f26a91bfd2575176db6a076968204d95
TCtiancheng <1005265162@qq.com>
2020-11-07 10:11:42 +0800

    优化中高职

--- src/project/protal-high-school/assets/js/table/highClass.js
+++ src/project/protal-high-school/assets/js/table/highClass.js
@@ -10,7 +10,10 @@ const highClass = {
     {
       title: '年级',
       dataIndex: 'gradeName',
-      width: '12%'
+      width: '12%',
+      customRender: text => {
+        return text + '级'
+      }
     },
     {
       title: '专业',
